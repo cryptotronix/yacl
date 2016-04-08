cdfb2fa25b8ae3109cb2b4d4d634c3cd222751ef
Josh Datko <jbd@cryptotronix.com>
2016-04-08 11:03:44 -0600

    exports the symbols for guile.

--- src/guile/extension.h
+++ src/guile/extension.h
@@ -2,12 +2,14 @@
 #ifndef YACL_GUILE_EXT_H_
 #define YACL_GUILE_EXT_H_
 
-
+#include "../../yacl.h"
 #include <libguile.h>
 
+YACL_EXPORT
 SCM
 yacl_scm_sha256 (SCM bv);
 
+YACL_EXPORT
 void
 yacl_init_guile (void);
 
