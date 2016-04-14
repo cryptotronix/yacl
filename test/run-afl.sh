b2e8e1a6b92be3b3382549854ca7a5bf60602542
Josh Datko <jbd@cryptotronix.com>
2016-04-14 10:48:44 -0600

    fixes the script to be more flexible

--- test/run-afl.sh
+++ test/run-afl.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-if [ -d "/tmp/afl-ramdisk" ]
-   echo "afl-ramdisk already exists"
-else
-    mkdir /tmp/afl-ramdisk
-    chmod 777 /tmp/afl-ramdisk
-    sudo mount -t tmpfs -o size=1024M tmpfs /tmp/afl-ramdisk
+if [ "$#" -eq 0 ]; then
+    echo "usage: $0 RAMDISK_MNT"
+    exit 1
 fi
 
-AFL_SKIP_CPUFREQ=1 afl-fuzz -i b64urltest -o /tmp/afl-ramdisk ./afl
+AFL_RAMDISK=$1
+AFL_INPUT_DIR=$2
+AFL_TEST_PROG=$3
+
+if [ -d "$AFL_RAMDISK" ]
+then   
+   echo "afl-ramdisk already exists"
+else
+    mkdir $AFL_RAMDISK
+    chmod 777 $AFL_RAMDISK
+    sudo mount -t tmpfs -o size=1024M tmpfs $AFL_RAMDISK
+fi
+
+AFL_SKIP_CPUFREQ=1 afl-fuzz -i $AFL_INPUT_DIR -o $AFL_RAMDISK $AFL_TEST_PROG
